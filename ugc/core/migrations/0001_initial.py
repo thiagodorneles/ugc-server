546bd0fd91930ba9e2335f807813630a44157131
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-11-16 19:47:47 -0200

    Adicionando migrations (South)

--- ugc/core/migrations/0001_initial.py
+++ ugc/core/migrations/0001_initial.py
@@ -8,13 +8,35 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        # Adding model 'Publish'
+        db.create_table(u'core_publish', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')()),
+            ('location', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('city', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
+            ('status', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'core', ['Publish'])
+
 
     def backwards(self, orm):
-        pass
+        # Deleting model 'Publish'
+        db.delete_table(u'core_publish')
+
 
     models = {
-        
+        u'core.publish': {
+            'Meta': {'ordering': "['-created_at']", 'object_name': 'Publish'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'status': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
     }
 
     complete_apps = ['core']
