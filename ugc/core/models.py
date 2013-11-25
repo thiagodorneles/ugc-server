38d83649a822235b1bb898f4451a06b3e68b2d1a
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-11-24 23:31:05 -0200

    Incluindo colunas para quantidade de visualização e bloqueios

--- ugc/core/models.py
+++ ugc/core/models.py
@@ -3,13 +3,15 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 class Publish(models.Model):
-    title       = models.CharField(_('Titulo'), max_length=100)
-    description = models.TextField(_('Descricao'))
-    created_at  = models.DateTimeField(_('Criado em'), auto_now_add=True)
-    location    = models.CharField(_('Localizacao'), max_length=50, blank=True)
-    city        = models.CharField(_('Cidade'), max_length=100, blank=True)
-    status      = models.BooleanField(_('Status'), default=True)
-    tags        = models.ManyToManyField('Tag', verbose_name=_('Tag'), related_name='publish', symmetrical=False)
+    title        = models.CharField(_('Titulo'), max_length=100)
+    description  = models.TextField(_('Descricao'))
+    created_at   = models.DateTimeField(_('Criado em'), auto_now_add=True)
+    location     = models.CharField(_('Localizacao'), max_length=50, blank=True)
+    city         = models.CharField(_('Cidade'), max_length=100, blank=True)
+    status       = models.BooleanField(_('Status'), default=True)
+    tags         = models.ManyToManyField('Tag', verbose_name=_('Tag'), related_name='publish', symmetrical=False)
+    quant_views  = models.IntegerField(_('Visualizacoes'), default=0)
+    quant_blocks = models.IntegerField(_('Bloqueios'), default=0)
     
     class Meta:
         unique_together = ('title', 'description')
@@ -23,7 +25,7 @@ class Publish(models.Model):
 class Tag(models.Model):
     tag        = models.CharField(_('Tag'), max_length=30, unique=True)
     created_at = models.DateTimeField(_('Criado em'), auto_now_add=True)
-    status = models.BooleanField(_(u'Situação'), default=True)
+    status     = models.BooleanField(_(u'Situação'), default=True)
 
     class Meta:
         ordering = ['tag']
