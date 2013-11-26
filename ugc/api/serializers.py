cc715118061eb85bc5ae39d042a64bff4093a5f0
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-11-26 17:35:19 -0200

    Modificando serialização de User e Tags para objeto Publish

--- ugc/api/serializers.py
+++ ugc/api/serializers.py
@@ -1,16 +1,24 @@
 # coding: utf-8
 from rest_framework import serializers
-from ugc.core.models import Publish, Tag
+from ugc.core.models import Publish, Tag, User
 
 class PublishSerializer(serializers.ModelSerializer):
-    tags = serializers.RelatedField(many=True)
-    user = serializers.RelatedField()
+    tags = serializers.SlugRelatedField(many=True, slug_field='tag')
+    user = serializers.SlugRelatedField(slug_field='id')
+    user_name = serializers.Field(source = 'user_name')
+    
     class Meta:
         model = Publish
     	resource_name = 'publishs'
-        fields = ('title', 'description', 'created_at', 'location', 'city', 'status', 'tags', 'user')
+        fields = ('title', 'description', 'created_at', 'location', 'city', 'status', 'tags', 
+                  'user', 'user_name', 'quant_views', 'quant_blocks', )
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ('tag', 'created_at', 'status')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('name', 'email', 'created_at')
