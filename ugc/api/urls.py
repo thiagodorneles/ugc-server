615f5b0fb33ea81cc0c31a715da789aec994f32f
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-12-04 23:45:01 -0200

    View para solicitar bloqueio de uma publicação

--- ugc/api/urls.py
+++ ugc/api/urls.py
@@ -9,6 +9,7 @@ router.register(r'api/tags', views.TagViewSet)
 router.register(r'api/users', views.UserViewSet)
 
 urlpatterns = patterns('',
+    url(r'^api/block/', 'ugc.api.views.publish_block', name='publish_block'),
     url(r'^api/$', 'ugc.api.views.api_root'),
     url(r'^', include(router.urls)),
 )
--- ugc/api/views.py
+++ ugc/api/views.py
@@ -1,13 +1,17 @@
 # coding: utf-8
-from ugc.core.models import Publish, Tag, User
-from ugc.api.serializers import PublishSerializer, TagSerializer, UserSerializer
+from django.http import HttpResponse
+# from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import status
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from datetime import datetime
 from geopy.geocoders import GoogleV3
+from ugc.core.models import Publish, Tag, User
+from ugc.api.serializers import PublishSerializer, TagSerializer, UserSerializer
 
 class PublishViewSet(mixins.CreateModelMixin, 
                      mixins.ListModelMixin,
@@ -16,7 +20,6 @@ class PublishViewSet(mixins.CreateModelMixin,
     base_name = 'publishs-list'
     queryset = Publish.objects.all().filter(status=True)
     serializer_class = PublishSerializer
-
 
     def retrieve(self, request, *args, **kwargs):
         """
@@ -72,6 +75,20 @@ class UserViewSet(mixins.CreateModelMixin,
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+# @csrf_exempt
+@api_view(['POST'])
+def publish_block(request):
+    print '================ AQUI ==================\n'
+    pk = request.DATA.get('id')
+
+    try:
+        publish = Publish.objects.get(pk=pk)
+    except Publish.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = PublishSerializer(publish, data=request.DATA)
+    serializer.object.update_block()
+    return Response(serializer.data)
 
 # @api_view(('GET',))
 # def api_root(request, format=None):
