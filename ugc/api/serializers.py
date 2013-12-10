8c5929923cd4b7e4c85b7e732966a760eb55878d
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-12-09 23:47:57 -0200

    API de Publishs para devolver urls das imagens

--- ugc/api/serializers.py
+++ ugc/api/serializers.py
@@ -1,17 +1,19 @@
 # coding: utf-8
 from rest_framework import serializers
-from ugc.core.models import Publish, Tag, User
+from ugc.core.models import Publish, Tag, User, Media
 
 class PublishSerializer(serializers.ModelSerializer):
     tags = serializers.SlugRelatedField(many=True, slug_field='tag')
     user = serializers.SlugRelatedField(slug_field='id')
     user_name = serializers.Field(source = 'user_name')
+    images = serializers.RelatedField(many=True, source='media_set')
+    thumbs = serializers.Field(source='thumbs')
     
     class Meta:
         model = Publish
     	resource_name = 'publishs'
         fields = ('id', 'title', 'description', 'created_at', 'location', 'city', 'status', 'tags', 
-                  'user', 'user_name', 'quant_views', 'quant_blocks', )
+                  'user', 'user_name', 'quant_views', 'quant_blocks', 'images', 'thumbs', )
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
@@ -31,3 +33,8 @@ class UserSerializer(serializers.ModelSerializer):
                   'twitter_user', 'twitter_id', 'twitter_token', 
                   'facebook_user', 'facebook_id', 'facebook_token',
                   'publishs')
+
+class MediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Media
+        fields = ('image', 'thumbnail', )
