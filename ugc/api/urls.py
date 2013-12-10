adea19c210bae5849c8d98ab6975c7d1d70414bb
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-12-09 23:48:17 -0200

    Inserida rota para upload de imagens

--- ugc/api/urls.py
+++ ugc/api/urls.py
@@ -7,9 +7,11 @@ router = DefaultRouter()
 router.register(r'api/publishs', views.PublishViewSet)
 router.register(r'api/tags', views.TagViewSet)
 router.register(r'api/users', views.UserViewSet)
+# router.register(r'api/upload', views.PublishUploadViewSet)
 
 urlpatterns = patterns('',
     url(r'^api/block/', 'ugc.api.views.publish_block', name='publish_block'),
+    url(r'^api/upload/', 'ugc.api.views.publish_image', name='publish_image'),
     url(r'^api/$', 'ugc.api.views.api_root'),
     url(r'^', include(router.urls)),
 )
