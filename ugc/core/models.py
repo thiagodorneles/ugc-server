fdc6b60c905e5f9aed49e7e7d00d52debf8bbe71
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-12-09 23:49:59 -0200

    colunas para salvar imagens e tratamento para determinar local do arquivo final

--- ugc/core/models.py
+++ ugc/core/models.py
@@ -2,6 +2,27 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse as r
+from datetime import datetime
+import os
+from uuid import uuid4
+# from south.modelsinspector import add_introspection_rules
+# from django_thumbs.db.models import ImageWithThumbsField
+
+# add_introspection_rules(
+#     [
+#         (
+#             (ImageWithThumbsField, ),
+#             [],
+#             {
+#                 "verbose_name": ["verbose_name", {"default": None}],
+#                 "name":         ["name",         {"default": None}],
+#                 "width_field":  ["width_field",  {"default": None}],
+#                 "height_field": ["height_field", {"default": None}],
+#                 "sizes":        ["sizes",        {"default": None}],
+#             },
+#         ),
+#     ],
+#     ["^django_thumbs\.db\.models\.ImageWithThumbsField",])
 
 
 class User(models.Model):
@@ -66,6 +87,13 @@ class Publish(models.Model):
         """
         return self.user.name
 
+    @property
+    def thumbs(self):
+        medias = self.media_set.all()
+        if not medias:
+            return medias
+        return map(lambda w: w.thumbnail.url, medias)
+
 class Tag(models.Model):
     tag        = models.CharField(_('Tag'), max_length=30, unique=True)
     created_at = models.DateTimeField(_('Criado em'), auto_now_add=True)
@@ -78,16 +106,92 @@ class Tag(models.Model):
     
     def __unicode__(self):
         return self.tag
-    
+
+
+
+def path_and_rename(thumb=False):
+    def wrapper(instance, filename):
+        ext = 'png'
+        if not thumb:
+            filename = '{}.{}'.format(uuid4().hex, ext)
+        else:
+            filename = '{}_thumbnail.{}'.format(uuid4().hex, ext)
+        path = 'publishs/{}/'.format(datetime.today().strftime("%Y/%m/%d"))
+        return os.path.join(path, filename)
+    return wrapper
+
 class Media(models.Model):
-    
-#     content = models.URLField(_('Arquivo'))
-#     content_type = models.CharField(_('Tipo'), max_length=100)
-#     publish = models.ForeignKey('Publish')
+    # image = ImageWithThumbsField(upload_to=path_and_rename(), max_length=100, default='', sizes=((126,91)))
+    image = models.ImageField(upload_to=path_and_rename(), max_length=100, default='')
+    thumbnail = models.ImageField(upload_to=path_and_rename(True), max_length=100, default='')
+    publish = models.ForeignKey('Publish', verbose_name=_(u'Publicação'), default=2)
 
     class Meta:
         verbose_name = _(u'Arquivo')
         verbose_name_plural = _(u'Arquivos')
+
+    def thumb_url(self):
+        return self.thumbnail.url
     
-#     def __unicode__(self):
-#         return content
+    def __unicode__(self):
+        return self.image.url
+
+    def create_thumbnail(self):
+        # original code for this method came from
+        # http://snipt.net/danfreak/generate-thumbnails-in-django-with-pil/
+ 
+        # If there is no image associated with this.
+        # do not create thumbnail
+        if not self.image:
+            return
+ 
+        from PIL import Image
+        from cStringIO import StringIO
+        from django.core.files.uploadedfile import SimpleUploadedFile
+        
+        # Set our max thumbnail size in a tuple (max width, max height)
+        THUMBNAIL_SIZE = (126,91)
+ 
+        DJANGO_TYPE = self.image.file.content_type
+ 
+        if DJANGO_TYPE == 'image/jpeg':
+            PIL_TYPE = 'jpeg'
+            FILE_EXTENSION = 'jpg'
+        elif DJANGO_TYPE == 'image/png':
+            PIL_TYPE = 'png'
+            FILE_EXTENSION = 'png'
+ 
+        # Open original photo which we want to thumbnail using PIL's Image
+        image = Image.open(StringIO(self.image.read()))
+ 
+        # Convert to RGB if necessary
+        # Thanks to Limodou on DjangoSnippets.org
+        # http://www.djangosnippets.org/snippets/20/
+        #
+        # I commented this part since it messes up my png files
+        #
+        #if image.mode not in ('L', 'RGB'):
+        #    image = image.convert('RGB')
+ 
+        # We use our PIL Image object to create the thumbnail, which already
+        # has a thumbnail() convenience method that contrains proportions.
+        # Additionally, we use Image.ANTIALIAS to make the image look better.
+        # Without antialiasing the image pattern artifacts may result.
+        image.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
+ 
+        # Save the thumbnail
+        temp_handle = StringIO()
+        image.save(temp_handle, PIL_TYPE)
+        temp_handle.seek(0)
+ 
+        # Save image to a SimpleUploadedFile which can be saved into
+        # ImageField
+        suf = SimpleUploadedFile(os.path.split(self.image.name)[-1], temp_handle.read(), content_type=DJANGO_TYPE)
+        # Save SimpleUploadedFile into image field
+        self.thumbnail.save('%s_thumbnail.%s'%(os.path.splitext(suf.name)[0],FILE_EXTENSION), suf, save=False)
+ 
+    def save(self):
+        # create a thumbnail
+        self.create_thumbnail()
+ 
+        super(Media, self).save()
