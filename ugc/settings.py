f07570281718a08e73fc2992d1e5cb4e374acd0d
Thiago Dorneles <thiagodorneles@Thiagos-MacBook-Pro.local>
2016-06-09 20:35:05 -0300

    fix

--- ugc/settings.py
+++ ugc/settings.py
@@ -168,7 +168,7 @@ LOGGING = {
         'console': {
             'level':'DEBUG',
             'filters': ['require_debug_true'],
-            'class':'logging.StreamHandler' 
+            'class':'logging.StreamHandler'
         }
     },
     'loggers': {
@@ -184,9 +184,9 @@ SOUTH_TESTS_MIGRATE = False
 
 # Email
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'thiagodornelesrs@gmail.com'
-EMAIL_HOST_PASSWORD = 'th#2go29'
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_SUBJECT_PREFIX = '[vc reporter]'
 
